a2aa771030f461ac20652135d2796eadff4a44af
IldaRon <de.ikar@yandex.ru>
2026-02-09 21:25:22 +0000

    Update 1.Connect_API_FitBit_ChatGPT.py

--- Virtual_host/backend/1.Connect_API_FitBit_ChatGPT.py
+++ Virtual_host/backend/1.Connect_API_FitBit_ChatGPT.py
@@ -8,15 +8,11 @@ import gather_keys_oauth2 as oauth2
 import fitbit
 from datetime import datetime, timedelta
 load_dotenv()
-# ---------- CONFIG ----------
-#CLIENT_ID = os.getenv('23TRFB')
-#CLIENT_SECRET = os.getenv("15eeaa771aedb966c5a700d42086c64c")
-#REDIRECT_URI = os.getenv("http://127.0.0.1:8080/")
 
-CLIENT_ID = '23TRFB'
-CLIENT_SECRET = '15eeaa771aedb966c5a700d42086c64c'
+CLIENT_ID = 'CLIENT_ID'                   # Name from FitBit API 
+CLIENT_SECRET = 'CLIENT_SECRET'           # From FitBit API 
 REDIRECT_URI = 'http://127.0.0.1:8080/'
-openai_client = OpenAI(api_key="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")
+openai_client = OpenAI(api_key="api_key") # api_key
 
 app = FastAPI()
 
